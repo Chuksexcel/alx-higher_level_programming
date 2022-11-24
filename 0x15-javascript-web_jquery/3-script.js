79baedf0a87fbbb13d903c4f52c12070e567d3fe
Chukwuemeka Samuel Echeta <mekabanz@gmail.com>
2022-11-23 23:58:49 -0800

    fix

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,5 @@
+/* a JavaScript script that adds the class red to the <header> element when the user clicks on the tag DIV#red_header */
+const $ = window.$;
+$('#red_header').click(() => {
+  $('#red_header').addClass('red');
+});
